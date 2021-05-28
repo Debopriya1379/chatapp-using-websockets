a01a2044c59fd49e9d086409fdf96efd8a7d022a
Dobopriya1379 <papai7909@gmail.com>
2021-05-28 13:22:34 +0530

    emitting events and getting message text and displayintg it in console

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket =io();
+
+//message from server
+socket.on('message', msg=>{
+    console.log(msg);
+});
+
+//sub,it message
+chatForm.addEventListener('submit', e=>{
+    e.preventDefault();
+
+    //getting message text
+    const msg = e.target.elements.msg.value;
+
+    //emit message text to server
+    socket.emit('chatMessage', msg);
+
+    //clear input
+    e.target.elements.msg.value='';
+    e.target.elements.msg.focus();
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const { message } = require('statuses');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +14,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
     console.log('New User Connected');
+
+    //Welcome new user
+    socket.emit('message', 'welcome to letsChat');
+
+    //broadcast when a new user connects
+    socket.broadcast.emit('message', 'A new user has joined the chat');
+
+    //listen to chatMessage
+    socket.on('chatMessage', (msg) => {
+        console.log(msg);
+        io.emit('message', msg);
+    })
+
+    //runs when user disconnects
+    socket.on('disconnect', () => {
+        console.log('A user has left');
+        io.emit('message', 'A user has left the chat');
+    })
 });
+
 
 const port = 4000;
 
